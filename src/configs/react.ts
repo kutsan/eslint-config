6ef6a0d958d9726be812fdea50113d8642b0d529
Kutsan Kaplan <me@kutsan.dev>
2024-12-05 23:14:47 +0300

    feat: add react support

--- src/configs/react.ts
+++ src/configs/react.ts
@@ -1,8 +1,9 @@
-// import pluginReact from 'eslint-plugin-react';
-// import pluginReactHooks from 'eslint-plugin-react-hooks';
-// import pluginReactRefresh from 'eslint-plugin-react-refresh';
+import type { ESLint } from 'eslint'
+import pluginReact from 'eslint-plugin-react'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
+
 import type { ConfigArray } from '../types/config.js'
-import { tsxGlob } from '../utils/files-globs.js'
+import { tsGlob, tsxGlob } from '../utils/files-globs.js'
 import { reactTypeScriptLanguageOptions } from '../utils/language-options.js'
 
 export const reactConfig: ConfigArray = [
@@ -10,29 +11,27 @@ export const reactConfig: ConfigArray = [
     files: [tsxGlob],
     languageOptions: reactTypeScriptLanguageOptions,
   },
-  // {
-  //   files: [tsGlob, tsxGlob],
-  //   plugins: {
-  //     react: pluginReact as ESLint.Plugin,
-  //     'react-hooks': pluginReactHooks
-  //   },
-  //   settings: {
-  //     react: {
-  //       version: "detect",
-  //     },
-  //   },
-  //   rules: {
-  //     ...pluginReact.configs.flat.recommended.rules,
-  //     ...pluginReactHooks.configs.recommended.rules,
-  //   },
-  // },
-  // {
-  //   files: [tsxGlob],
-  //   plugins: {
-  //     'react-refresh': pluginReactRefresh,
-  //   },
-  //   rules: {
-  //     "react-refresh/only-export-components": "error",
-  //   },
-  // }
+  {
+    files: [tsGlob, tsxGlob],
+    plugins: {
+      react: pluginReact as ESLint.Plugin,
+      'react-hooks': pluginReactHooks as ESLint.Plugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      ...pluginReact.configs.flat!.recommended!.rules,
+      ...pluginReactHooks.configs.recommended?.rules,
+    },
+  },
+  {
+    files: [tsxGlob],
+    rules: {
+      'react-refresh/only-export-components': 'error',
+    },
+  },
 ]
