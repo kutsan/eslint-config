337330d71fc8d5f084205460366074a586abfd71
Kutsan Kaplan <me@kutsan.dev>
2026-04-18 18:54:24 +0300

    build: upgrade packages

--- src/configs/yaml.ts
+++ src/configs/yaml.ts
@@ -4,8 +4,7 @@ import type { Config } from 'eslint/config'
 import { yamlGlob } from '../utils/files-globs.ts'
 
 export const yamlConfig: Config[] = [
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion -- The plugin does not use eslint/config types.
-  ...(pluginYaml.configs['flat/recommended'] as Config[]).map((yamlConfig) => ({
+  ...pluginYaml.configs['flat/recommended'].map((yamlConfig) => ({
     ...yamlConfig,
     files: [yamlGlob],
   })),
